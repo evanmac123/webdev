7e6e83354655a9f31f475f3dfee8070fe7e3d866
Evan McEldowney <evanmac123@gmail.com>
2017-02-22 17:38:53 -0500

    heroku 3

--- test/app.js
+++ test/app.js
@@ -7,14 +7,15 @@ module.exports = function(app)
     var connectionString = 'mongodb://127.0.0.1:27017/test';
 
     if(process.env.MLAB_USERNAME_WEBDEV) {
-        var username = "evanmac";//process.env.MLAB_USERNAME_WEBDEV;
-        var password = "romeo123"; //process.env.MLAB_PASSWORD_WEBDEV;
+        var username = "evantest";//process.env.MLAB_USERNAME_WEBDEV;
+        var password = "evantest"; //process.env.MLAB_PASSWORD_WEBDEV;
 
         connectionString = 'mongodb://'+
             username + ':' +
             password +
             '@ds147975.mlab.com:47975/mceldowney_e_web_dev';
     }
+
     console.log(connectionString);
 
     var mongoose = require("mongoose");  // npm install mongoose --save
